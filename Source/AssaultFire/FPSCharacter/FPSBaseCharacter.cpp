1f218cb8dda25cfd0748e3bf7701ffb89efb9fe6
Backfire <13866816549@163.com>
2023-03-20 22:34:53 +0800

    1.增强输入绑定，实现移动旋转 2.实现鼠标旋转摄像机 3.实现玩家跳跃功能 4.第三人称动画导入，动画重定向 5.创建动画蓝图和BlendSpace 6.RPC问题的解决，实现shift服务器客户端同步速度 7.导入玩家第一人称手臂动画蓝图及混合空间

--- Source/AssaultFire/FPSCharacter/FPSBaseCharacter.cpp
+++ Source/AssaultFire/FPSCharacter/FPSBaseCharacter.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#pragma once
 #include "FPSBaseCharacter.h"
-
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
 // Sets default values
 AFPSBaseCharacter::AFPSBaseCharacter()
 {
@@ -13,7 +14,7 @@ AFPSBaseCharacter::AFPSBaseCharacter()
 	PlayerCamera->SetupAttachment(RootComponent);
 	
 	FPSArmsMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPSArmsMesh"));
-	FPSArmsMesh->SetupAttachment(RootComponent);
+	FPSArmsMesh->SetupAttachment(PlayerCamera);
 	//这个手臂模型是给第一人称的自己看的，别人看到就穿帮了
 	FPSArmsMesh->SetOnlyOwnerSee(true);
 
@@ -33,6 +34,103 @@ void AFPSBaseCharacter::BeginPlay()
 	
 }
 
+void AFPSBaseCharacter::MoveForward(const FInputActionValue& InputValue)
+{
+	float value = InputValue.GetMagnitude();
+
+	if((Controller != nullptr) && (value!= 0.f))
+	{
+		// 判断哪个旋转方向是向前的
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		// 获取向前的向量
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, value);
+	}
+
+}
+
+void AFPSBaseCharacter::MoveRight(const FInputActionValue& InputValue)
+{
+	float value = InputValue.GetMagnitude();
+
+	if(Controller != nullptr && value != 0.f)
+	{
+		// 判断哪个旋转方向是向右的
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		// 获取向右的向量
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, value);
+	}
+
+}
+
+void AFPSBaseCharacter::LookRightRate(const FInputActionValue& InputValue)
+{
+	AddControllerYawInput(InputValue.GetMagnitude() * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+}
+
+void AFPSBaseCharacter::LookUpRate(const FInputActionValue& InputValue)
+{
+	AddControllerPitchInput(InputValue.GetMagnitude() * TurnRateGamepad * GetWorld()->GetDeltaSeconds());
+}
+
+void AFPSBaseCharacter::OnJump(const FInputActionValue& InputValue)
+{
+	Jump();
+}
+
+void AFPSBaseCharacter::OnJumpStoping(const FInputActionValue& InputValue)
+{
+	StopJumping();
+}
+
+void AFPSBaseCharacter::Fire(const FInputActionValue& InputValue)
+{
+	FString Message = FString::SanitizeFloat(999);
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			60,
+			FColor::Green,
+			Message
+		);
+	}
+}
+
+void AFPSBaseCharacter::LowSpeedWalk(const FInputActionValue& InputValue)
+{
+	GetCharacterMovement()->MaxWalkSpeed = 300.f;
+	ServerLowSpeedWalk();
+}
+
+void AFPSBaseCharacter::NormalSpeedWalk(const FInputActionValue& InputValue)
+{
+	GetCharacterMovement()->MaxWalkSpeed = 450.f;
+	ServerNormalSpeedWalk();
+}
+
+void AFPSBaseCharacter::ServerLowSpeedWalk_Implementation()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 300.f;
+}
+
+bool AFPSBaseCharacter::ServerLowSpeedWalk_Validate()
+{
+	return true;
+}
+
+void AFPSBaseCharacter::ServerNormalSpeedWalk_Implementation()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 450.f;
+}
+
+bool AFPSBaseCharacter::ServerNormalSpeedWalk_Validate()
+{
+	return true;
+}
 
 void AFPSBaseCharacter::Tick(float DeltaTime)
 {
@@ -44,5 +142,47 @@ void AFPSBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	//使用控制器控制旋转pitch和yaw
+	bUseControllerRotationPitch = true;
+	bUseControllerRotationYaw = true;
+	
+	if(APlayerController * PC = Cast<APlayerController>(GetController()))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()) )
+		{
+			Subsystem->AddMappingContext(IMC_Action, 0);
+			Subsystem->AddMappingContext(IMC_MoveBase, 0);
+		}
+	}
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if(EnhancedInputComponent)
+	{
+
+		//移动
+		EnhancedInputComponent->BindAction(IA_MoveForward, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::MoveForward);
+		EnhancedInputComponent->BindAction(IA_MoveRight, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::MoveRight);
+		//旋转
+		EnhancedInputComponent->BindAction(IA_LookRightRate, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::LookRightRate);
+		EnhancedInputComponent->BindAction(IA_LookUpRate, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::LookUpRate);
+		//跳跃
+		EnhancedInputComponent->BindAction(IA_Jump, ETriggerEvent::Started, this, &AFPSBaseCharacter::OnJump);
+		EnhancedInputComponent->BindAction(IA_Jump, ETriggerEvent::Completed, this, &AFPSBaseCharacter::OnJumpStoping);
+		//射击
+		EnhancedInputComponent->BindAction(IA_Fire, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::Fire);
+		
+		//shift静步
+		EnhancedInputComponent->BindAction(IA_Shift, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::LowSpeedWalk);
+		EnhancedInputComponent->BindAction(IA_Shift, ETriggerEvent::Completed, this, &AFPSBaseCharacter::NormalSpeedWalk);
+
+	}
+
+	// 你可以通过更改"ETriggerEvent"枚举值，绑定到此处的任意触发器事件
+	//Input->BindAction(AimingInputAction, ETriggerEvent::Triggered, this, &AFPSBaseCharacter::SomeCallbackFunc);
 }
+
+
+
+
 
--- Source/AssaultFire/FPSCharacter/FPSBaseCharacter.h
+++ Source/AssaultFire/FPSCharacter/FPSBaseCharacter.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include"InputActionValue.h"
 #include "Camera/CameraComponent.h"
 #include "FPSBaseCharacter.generated.h"
 
@@ -22,20 +25,100 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = Character, meta = (AllowPrivateAccess = "true"))
 		USkeletalMeshComponent * FPSArmsMesh;
 
+	//增强输入
+	//两个映射表
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "EnhancedInput | Context",meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputMappingContext> IMC_Action;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "EnhancedInput | Context", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputMappingContext> IMC_MoveBase;
+
+	//上下移动
+	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction> IA_MoveForward;
+
+	//左右移动
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction> IA_MoveRight;
+
+	//上下视角
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction> IA_LookUpRate;
+
+	//左右视角
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction>  IA_LookRightRate;
+
+	//跳跃
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction> IA_Jump;
+
+	//开火
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction> IA_Fire;
+
+	//按下shift静步减速
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnhancedInput | Action", meta = (AllowPrivateAccess = "true"))
+		TObjectPtr<UInputAction> IA_Shift;
+
+
+	//游戏内鼠标灵敏度
+	UPROPERTY(EditAnywhere, Category = "Input")
+	float TurnRateGamepad = 5.f;
+
+public:
+
+protected:
+
+private:
+
+
+protected:
+
+	
+	virtual void BeginPlay() override;
+
+	///输入绑定
+	void MoveForward(const FInputActionValue& InputValue);
+
+	void MoveRight(const FInputActionValue& InputValue);
+
+	void LookRightRate(const FInputActionValue& InputValue);
+
+	void LookUpRate(const FInputActionValue& InputValue);
+
+	void OnJump(const FInputActionValue& InputValue);
+
+	void OnJumpStoping(const FInputActionValue& InputValue);
+
+	void Fire(const FInputActionValue& InputValue);
+
+	//按下左shift静步慢走
+	void LowSpeedWalk(const FInputActionValue& InputValue);
+
+	//松开左shift正常行走
+	void NormalSpeedWalk(const FInputActionValue& InputValue);
+	///
+
 
 public:
 
 	AFPSBaseCharacter();
 
-protected:
-
-	virtual void BeginPlay() override;
-
-public:	
-
 	virtual void Tick(float DeltaTime) override;
 
-
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//RPC角色减速
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerLowSpeedWalk();
+	void ServerLowSpeedWalk_Implementation();
+	bool ServerLowSpeedWalk_Validate();
+
+	//RPC角色恢复速度
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerNormalSpeedWalk();
+	void ServerNormalSpeedWalk_Implementation();
+	bool ServerNormalSpeedWalk_Validate();
 
 };
