1935374bc907a602d39b1a2b196b899789c2feef
Backfire <13866816549@163.com>
2023-03-20 12:51:12 +0800

    同步

--- Source/AssaultFire/FPSCharacter/FPSBaseCharacter.cpp
+++ Source/AssaultFire/FPSCharacter/FPSBaseCharacter.cpp
@@ -6,10 +6,9 @@
 // Sets default values
 AFPSBaseCharacter::AFPSBaseCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-#pragma region Component
+
 	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
 	PlayerCamera->SetupAttachment(RootComponent);
 	
@@ -23,26 +22,24 @@ AFPSBaseCharacter::AFPSBaseCharacter()
 	//模型不需要碰撞，碰撞后面由胶囊体处理
 	GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	GetMesh()->SetCollisionObjectType(ECC_Pawn);
-#pragma region Component
+
 
 	
 }
 
-// Called when the game starts or when spawned
 void AFPSBaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
+
 void AFPSBaseCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AFPSBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
